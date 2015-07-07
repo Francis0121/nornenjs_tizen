a377279dd73f3a09bc5ff53ed4d973601adc8953
Francis0121 <tjdrms0121@gmail.com>
2015-07-07 13:21:00 +0900

    remove elementary gles code

--- nornenjs/src/volume_render.c
+++ nornenjs/src/volume_render.c
@@ -1,25 +1,6 @@
-/*
- * Copyright (c) 2013 Samsung Electronics Co., Ltd All Rights Reserved
- *
- * Licensed under the Apache License, Version 2.0 (the License);
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an AS IS BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include <Elementary_GL_Helpers.h>
 #include "nornenjs.h"
 #include "opengles.h"
 #include "socket_io_client.hpp"
-
-ELEMENTARY_GLVIEW_GLOBAL_DEFINE();
 
 static pthread_t thread_id;
 
@@ -40,7 +21,6 @@ static void glview_create(appdata_s *ad){
    Evas_Object *render_view;
 
    ad->render_view = render_view = elm_glview_version_add(ad->box, EVAS_GL_GLES_1_X);
-   ELEMENTARY_GLVIEW_GLOBAL_USE(ad->render_view);
    evas_object_data_set(render_view, APPDATA_KEY, ad);
 
    elm_glview_mode_set(render_view, ELM_GLVIEW_ALPHA | ELM_GLVIEW_DEPTH);
